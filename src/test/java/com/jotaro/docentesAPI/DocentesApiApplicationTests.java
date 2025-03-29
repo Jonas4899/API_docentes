0112736c18690b30ea33a1279ae1659d43e17b8f
Jonathan <jonatandavidsal@gmail.com>
2025-03-28 20:26:03 -0500

    Añadir archivos de configuracion inicial SpringBoot

---
+++ src/test/java/com/jotaro/docentesAPI/DocentesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jotaro.docentesAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocentesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
